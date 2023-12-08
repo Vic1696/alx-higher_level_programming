834c75116ba7a47c7f63d4b81635cc5c617e3ec0
Vic1696 <98239821+Vic1696@users.noreply.github.com>
2023-12-08 21:47:12 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/python3
+def square_matrix_simple(matrix=None):
+    #Check if matrix = None. 
+    #If None, assign an empty list
+    if matrix is None:
+        matrix = []
+    
+    # Use a list comprehension for clarity and conciseness
+    new_matrix = [[x**2 for x in row] for row in matrix]
+    
+    return new_matrix
